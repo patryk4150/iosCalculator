39ae82581e5aacd35c2ace249bf8b54089403dcc
Patryk Cyrek <patryk.cyrek@yahoo.com>
2019-10-14 16:22:25 +0200

    Initial Commit

---
+++ Calculator_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_iOS
+//
+//  Created by Patryk Cyrek on 14/10/2019.
+//  Copyright © 2019 Patryk Cyrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
